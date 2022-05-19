7b3b3086e91c6d11bc68b796c1c9449099fe44d9
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-05-19 23:53:45 +0600

    76

--- index.js
+++ index.js
@@ -43,10 +43,23 @@ const run = async () => {
     const serverCollation = client.db("doctors_portal").collection("services");
     const bookingCollation = client.db("doctors_portal").collection("booking");
     const userCollation = client.db("doctors_portal").collection("user");
+    const doctorCollation = client.db("doctors_portal").collection("doctors");
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollation.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
 
     app.get("/service", async (req, res) => {
       const query = {};
-      const cursor = serverCollation.find(query);
+      const cursor = serverCollation.find(query).project({ name: 1 });
       const services = await cursor.toArray();
       res.send(services);
     });
@@ -68,23 +81,14 @@ const run = async () => {
     });
 
     // admin rule
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollation.findOne({
-        email: requester,
-      });
-      if (requesterAccount.role === "admin") {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
-        const result = await userCollation.updateOne(filter, updateDoc);
-
-        res.send(result);
-      } else {
-        res.status(403).send({ message: "forbidden" });
-      }
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollation.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     app.put("/user/:email", async (req, res) => {
@@ -173,6 +177,24 @@ const run = async () => {
       }
       const result = await bookingCollation.insertOne(booking);
       return res.send({ success: true, result });
+    });
+
+    app.get("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctors = await doctorCollation.find().toArray();
+      res.send(doctors);
+    });
+
+    app.post("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollation.insertOne(doctor);
+      res.send(result);
+    });
+
+    app.delete("/doctor/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await doctorCollation.deleteOne(filter);
+      res.send(result);
     });
   } finally {
   }
